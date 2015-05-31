6b104ef672f5e35981f68bc843980e2917893e4c
leigin <23335096@qq.com>
2015-05-31 18:13:31 +0800

    新增注释

--- yum_conf_webtatic_centos5.sh
+++ yum_conf_webtatic_centos5.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+#这个yum源主要是对最新版的mysql进行yum支持
 echo "=====start check yum ENV====="
 if [ -d "/etc/yum.repos.d" ];then
 	rpm -Uvh http://mirror.webtatic.com/yum/el5/latest.rpm
--- yum_conf_webtatic_centos6.sh
+++ yum_conf_webtatic_centos6.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+#这个yum源主要是对最新版的mysql进行yum支持
 echo "=====start check yum ENV====="
 if [ -d "/etc/yum.repos.d" ];then
 	rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
