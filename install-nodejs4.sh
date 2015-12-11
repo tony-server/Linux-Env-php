02d736499f1cf16180c2f35429e9bf9c3b4e3323
root <root@iZ94ztksb37Z.(none)>
2015-12-11 12:32:39 +0800

    modify

--- install-nodejs4.sh
+++ install-nodejs4.sh
@@ -5,6 +5,7 @@ wget https://nodejs.org/dist/v4.2.3/node-v4.2.3-linux-x64.tar.gz
 tar zxvf node-v4.2.3-linux-x64.tar.gz
 mv node-v4.2.3-linux-x64 /usr/local/nodejs42
 
+sed -i '$a \ ' /etc/profile
 sed -i '$a export PATH=$PATH:/usr/local/nodejs42/bin' /etc/profile
 
 
