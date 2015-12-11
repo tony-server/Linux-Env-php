0e3c9e7a02a6fa3e6c7063e8ce8410aeab7e9ca5
root <root@iZ94ztksb37Z.(none)>
2015-12-11 23:43:13 +0800

    modify redis.sh

--- install-redis28.sh
+++ install-redis28.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+cd /root/src
+wget https://github.com/antirez/redis/archive/2.8.22.tar.gz
+cd redis-2.8.22
 make && make PREFIX=/usr/local/redis install
+cp redis.conf /usr/local/redis/default.redis.conf
+
