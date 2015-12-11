2122602042a88c9cd6893c1e7fb9a3502235a96a
root <root@iZ94ztksb37Z.(none)>
2015-12-11 23:45:58 +0800

    redis

--- install-redis28.sh
+++ install-redis28.sh
@@ -5,4 +5,4 @@ wget https://github.com/antirez/redis/archive/2.8.22.tar.gz
 cd redis-2.8.22
 make && make PREFIX=/usr/local/redis install
 cp redis.conf /usr/local/redis/default.redis.conf
-
+# 启动：		/usr/local/redis/bin/redis-server /usr/local/redis/default.redis.conf 
