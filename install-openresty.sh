c47d66e653bf9bc92c5981f3c5afa0d6f956be48
root <root@iZ62cmd5g53Z.(none)>
2016-08-22 14:54:46 +0800

    update

--- install-openresty.sh
+++ install-openresty.sh
@@ -3,4 +3,9 @@
 # 安装依赖
 yum install readline-devel pcre-devel openssl-devel gcc
 
+
+# 添加模块
+# ./configure --prefix=/usr/local/openresty --add-module=/root/src/FastDFS/fastdfs-nginx-module-master/src
+
+
 ./configure --prefix=/usr/local/openresty
