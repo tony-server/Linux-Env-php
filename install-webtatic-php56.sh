3a7dcf18efb605d9a06b7d49ebfddb6f0bf8aabb
leigin <23335096@qq.com>
2017-04-17 10:44:21 +0800

    优化

--- install-webtatic-php56.sh
+++ install-webtatic-php56.sh
@@ -7,7 +7,9 @@ yum install -y nginx
 
 #安装 php
 yum -y install php56w php56w-opcache
-yum -y install php56w-bcmath  php56w-cli php56w-common php56w-dba php56w-devel php56w-embedded php56w-enchant php56w-fpm php56w-gd php56w-imap php56w-intl php56w-ldap php56w-mbstring php56w-mcrypt  php56w-mysqlnd php56w-odbc php56w-pdo php56w-pear php56w-pecl-apcu php56w-pecl-gearman php56w-pecl-geoip php56w-pecl-imagick  php56w-pecl-memcache php56w-pecl-xdebug php56w-pgsql php56w-phpdbg php56w-process php56w-pspell php56w-recode  php56w-snmp  php56w-soapphp56w-tidy php56w-xml php56w-xmlrpc
+yum -y install php56w-bcmath  php56w-cli php56w-common php56w-dba php56w-devel php56w-embedded php56w-enchant php56w-fpm php56w-gd php56w-imap php56w-intl php56w-ldap php56w-mbstring php56w-mcrypt php56w-mysqlnd php56w-odbc php56w-pdo php56w-pear php56w-pecl-apcu php56w-pecl-gearman php56w-pecl-geoip php56w-pecl-imagick php56w-pecl-memcache php56w-pecl-xdebug php56w-pgsql php56w-phpdbg php56w-process php56w-pspell php56w-recode php56w-snmp php56w-soap php56w-tidy php56w-xml php56w-xmlrpc
+
+
 
 
 # 安装yar扩展（新建yar.ini 放到/etc/php.d/中）
